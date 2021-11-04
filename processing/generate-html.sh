ab67d56a7ed33edacbbadca030fda72ca9c778f9
Andy Irving <andrew.irving@bodleian.ox.ac.uk>
2021-11-04 17:39:24 +0000

    bring up to date with latest processing
    remove references to missing authorities
    
    add a .gitignore

--- processing/generate-html.sh
+++ processing/generate-html.sh
@@ -3,8 +3,10 @@
 echo
 echo "Generating HTML for customized manuscript view..."
 
-# Change directory to the location of this script
-cd "${0%/*}"
+if [[ ! "$(pwd)" == *-mss/processing ]]; then
+    echo "This script must be run from the processing folder"
+    exit 1
+fi 
 
 # Create subfolder to keep generated files out of GitHub
 if [ ! -d "html" ]; then
@@ -16,4 +18,8 @@ LOGFILE="html/html.log"
 echo "Transforming TEI files in collections folder using convert2HTML.xsl on $(date +"%Y-%m-%d %H:%M:%S") to create HTML manuscript view pages." > $LOGFILE
 
 # Run XSLT on all TEI files in collections path (using pwd to get full path, not relative, which is what the XSL needs)
-java -Xmx1G -Xms1G -cp "saxon/saxon9he.jar"  net.sf.saxon.Transform -it:batch -xsl:convert2HTML.xsl collections-path=`pwd`/../collections/ 2>> $LOGFILE
+if ! java -Xmx1G -Xms1G -cp "saxon/saxon9he.jar"  net.sf.saxon.Transform -it:batch -xsl:convert2HTML.xsl collections-path="$(pwd)"/../collections/ 2>> $LOGFILE;
+then
+    echo "XSLT failed. Re-indexing cancelled. Please raise an issue on GitHub, attaching $LOGFILE"
+    exit 1;
+fi
